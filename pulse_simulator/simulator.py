fix leakage problem

pulse_simulator/simulator.py
@@ -13,12 +13,13 @@ class Simulator:
             system (System) : class for the target quantum system
             frame_frequency (float) : rotation frequency of the system simulating the time evolution
         """
-        system.compile(frame_frequency)
+        system.compile(frame_frequency)        
         self.dim = system.dim
-        self.static_hamiltonian = system.static_hamiltonian
-        self.operators = system.dynamic_operators
+        self.static_hamiltonian = system.static_hamiltonian_on_frame
+        self.operators = system.dynamic_operators_on_frame
         self.detunings = system.dynamic_detunings
-        self.frame = system.frame_difference
+        self.frame = system.frame_on_frame
+        self.comp = system.comp
         
     def set_sequence(self, sequence, step=0.1, visualize=False):
         """register the pulse sequence to be simulated
@@ -40,10 +41,9 @@ class Simulator:
         self.time = port.time
         self.trigger_position_list = sequence.trigger_position_list
 
-    def run(self, frame_inverse=True, return_all=True):
+    def run(self, return_all=True):
         """run the simulation
         Args:
-            frame_inverse (bool) : whether to correct the simulation results to the qubit frame
             return_all (float) : whether to return the simulation results during pulse execution
         """
 
@@ -90,7 +90,7 @@ class Simulator:
             unitary = [u]
             for h,s in zip(h_list, s_list):
                 u = lin.expm(-1j*h*s)@u
-                if return_all and frame_inverse:
+                if return_all:
                     f = lin.expm(+1j*frame*s)@f
                     unitary.append(f@u)
 
@@ -102,5 +102,4 @@ class Simulator:
                 return f@u
 
         t_list, s_list, h_list = precompile(2*np.pi*self.time, ith_hamiltonian)
-
         self.unitary = time_evolution(s_list, h_list, self.frame)

pulse_simulator/system.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy.sparse as sp
 from .util.transform import qubitize
 from .util.tensor_product import TensorProduct
+from .util.frame import get_system_dressed_frame
+from .util.leakage import get_computational_basis
 
 class Qubit:
     """Class for standard harmonic oscillator"""
@@ -265,20 +267,12 @@ class System:
             self.dynamic_operators[idx] = (operator_real, operator_imag)
             self.dynamic_detunings[idx] = d.frequency - frame_frequency
             
-        self.qubit_freqs = {}
-        for idx, q in self.qubits.items():
-            self.qubit_freqs[idx] = q.frequency
-        for idxs, c in self.coupls.items():
-            w0 = self.qubits[idxs[0]].frequency
-            w1 = self.qubits[idxs[1]].frequency
-            a0 = self.qubits[idxs[0]].anharmonicity
-            a1 = self.qubits[idxs[1]].anharmonicity
-            g01 = c.coupling
-            self.qubit_freqs[idxs[0]] += g01**2/(w0-w1) + g01**2*(a0+a1)/(((w0+a0)-w1)*(w0-(w1+a1)))
-            self.qubit_freqs[idxs[1]] += g01**2/(w1-w0) + g01**2*(a0+a1)/(((w0+a0)-w1)*(w0-(w1+a1)))
-            
-        self.frame_difference = 0
-        for idx, q in self.qubits.items():
-            tp = TensorProduct(*self.dims)
-            tp.prod(0.5*(self.qubit_freqs[idx]-frame_frequency)*q.Z, idx)
-            self.frame_difference += tp.get_operator()
+        self.conv, self.frame = get_system_dressed_frame(self)
+        self.comp = get_computational_basis(self)
+        
+        # operator conversion onto the system dressed frame
+        self.static_hamiltonian_on_frame = self.conv.T.conj()@self.static_hamiltonian@self.conv
+        self.dynamic_operators_on_frame = {}
+        for key, val in self.dynamic_operators.items():
+            self.dynamic_operators_on_frame[key] = self.conv.T.conj()@val@self.conv
+        self.frame_on_frame = self.conv.T.conj()@self.frame@self.conv